61194e4e42947a9832c866ee9f66494de0517e26
yusuke.nishimori <yusuke.nishimori@aakel.co.jp>
2019-04-30 10:38:40 +0000

    column add

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
     @tasks = Task.all
   end
   
   def show
-    @task = Task.find(params[:id])
   end
   
   def new
@@ -24,22 +25,19 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     if @task.update(task_params)
       flash[:success]='Taskは正常に更新されました'
       redirect_to @task
     else
-      flas.now[:danger]='Taskは更新されませんでした'
+      flash.now[:danger]='Taskは更新されませんでした'
       render :edit
     end
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     
     flash[:success]='Taskは正常に削除されました'
@@ -47,9 +45,12 @@ class TasksController < ApplicationController
   end
   
   private
-  
-  def task_params
-    params.require(:task).permit(:content)
+
+  def set_task
+    @task = Task.find(params[:id])
   end
   
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
 end
